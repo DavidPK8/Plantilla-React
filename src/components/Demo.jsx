fc335cfb994860424d95efa1dea7a1ab2dfc682c
DavidPK8 <alejodavicho8@gmail.com>
2024-01-29 16:45:47 -0500

    first commit

---
+++ src/components/Demo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hola from './Hola'
+
+export const Demo = () => {
+  return (
+    <div>Demo - Prueba de componente
+
+        <Hola></Hola>    
+    </div>
+  )
+}
+
+export default Demo
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,61 @@
 import Mensajes from "./Mensajes"
+import { useState } from "react"
+import { v4 as uuidv4 } from 'uuid';
 
-export const Formulario = () => {
+export const Formulario = ({ setEstado }) => {
+
+    const [error, setError] = useState(false)
+    const [mensaje, setMensaje] = useState(false)
+    const [form, setform] = useState({
+        nombre: "",
+        sector: "",
+        salida: "",
+        llegada: "",
+        maquinista: "",
+        detalles: ""
+    })
+
+    const handleChange = (e) => {
+        setform({
+            ...form,
+            [e.target.name]: e.target.value.trim()
+        })
+    } 
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (Object.values(form).includes("") || Object.entries(form).length === 0) {
+            setError(true)
+            setTimeout(() => {
+                setError(false)
+            }, 1000);
+            return
+        }
+        try {
+            const url = "http://localhost:3000/metro"
+            form.id = uuidv4()
+            await fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(form),
+                headers: { 'Content-Type': 'application/json' }
+            })
+            setMensaje(true)
+            setEstado(true)
+            setTimeout(() => {
+                setMensaje(false)
+                setEstado(false)
+                setform({})
+            }, 1000);
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
     return (
-        <form>
-            <Mensajes tipo={"bg-red-900"}>validar campos</Mensajes>
+        <form onSubmit={handleSubmit}>
+            {error && <Mensajes tipo="bg-red-900">"Existen campos vacíos"</Mensajes>}
+            {mensaje && <Mensajes tipo="bg-green-900">"Registro exitoso"</Mensajes>}
             <div>
                 <label
                     htmlFor='nombre'
@@ -14,6 +66,8 @@ export const Formulario = () => {
                     className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
                     placeholder='nombre de la ruta'
                     name='nombre'
+                    value={form.nombre || ""}
+                    onChange={handleChange}
                 />
             </div>
 
@@ -27,6 +81,8 @@ export const Formulario = () => {
                     className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
                     placeholder='sector de la ruta'
                     name='sector'
+                    value={form.sector || ""}
+                    onChange={handleChange}
                 />
             </div>
 
@@ -40,6 +96,8 @@ export const Formulario = () => {
                     className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
                     placeholder='punto de salida'
                     name='salida'
+                    value={form.salida || ""}
+                    onChange={handleChange}
                 />
             </div>
 
@@ -53,6 +111,8 @@ export const Formulario = () => {
                     className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
                     placeholder='punto de llegada'
                     name='llegada'
+                    value={form.llegada || ""}
+                    onChange={handleChange}
                 />
             </div>
 
@@ -66,6 +126,8 @@ export const Formulario = () => {
                     className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
                     placeholder='nombre del maquinista'
                     name='maquinista'
+                    value={form.maquinista || ""}
+                    onChange={handleChange}
                 />
             </div>
             <div>
@@ -77,14 +139,14 @@ export const Formulario = () => {
                     type="text"
                     className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
                     name='detalles'
+                    value={form.detalles || ""}
+                    onChange={handleChange}
                 />
             </div>
 
             <input
                 type="submit"
-                className='bg-sky-900 w-full p-3 
-        text-white uppercase font-bold rounded-lg 
-        hover:bg-red-900 cursor-pointer transition-all'
+                className='bg-sky-900 w-full p-3 text-white uppercase font-bold rounded-lg hover:bg-red-900 cursor-pointer transition-all'
                 value='Registrar ruta' />
 
         </form>
